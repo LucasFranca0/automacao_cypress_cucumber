addbafa8bad0bc5d8c9572a87a2d6afaa05afc71
Lucas Franca <lucascfranca11@gmail.com>
2025-03-25 22:46:03 -0300

    Ajuste: Ci

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esb
 module.exports = {
     projectId: process.env.CYPRESS_PROJECT_ID,
     e2e: {
+        baseUrl: 'https://www.saucedemo.com',
         specPattern: '**/*.feature',
         setupNodeEvents(on, config) {
             addCucumberPreprocessorPlugin(on, config);
@@ -13,6 +14,8 @@ module.exports = {
             }));
             return config;
         },
+        viewportWidth: 1280,
+        viewportHeight: 720,
         chromeWebSecurity: false // Os testes estavam falhando por causa do CORS, ficava esperando a resposta do servidor
     }
 };
